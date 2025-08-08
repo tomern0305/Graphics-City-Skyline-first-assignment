14a31e0fb78c62ddee1ddc157dd810ba0345767b
Tomer Natan <tomern0305@gmail.com>
2025-08-08 12:31:44 +0300

    windows in diffrest styles

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
@@ -42,12 +41,12 @@ FIREWORK fireworks[NUM_FIREWORKS];
 
 void init()
 {
-	int i,j;
-	glClearColor(0.0,0.0,0.3,0);// color of window background
+	int i, j;
+	glClearColor(0.0, 0.0, 0.3, 0);// color of window background
 	glOrtho(-1, 1, -1, 1, -1, 1); // set the coordinates system
 
 	srand(time(0));
-
+	//init stars in the sky
 	for (i = 0;i < NUM_STARS;i++)
 	{
 		stars[i].position.x = -1 + 2 * ((rand() % 1000) / 1000.0); // random value in [-1,1)
@@ -55,7 +54,7 @@ void init()
 		stars[i].size = 1 + rand() % 3;
 
 	}
-
+	//init fireworks
 	for (i = 0; i < NUM_FIREWORKS; i++)
 	{
 		fireworks[i].position.x = -1 + 2 * ((rand() % 1000) / 1000.0); // (-1,1)
@@ -65,7 +64,7 @@ void init()
 
 		for (j = 0; j < TAIL_LENGTH; j++)
 			fireworks[i].tail[j] = fireworks[i].position;
-		
+
 		for (j = 0; j < NUM_PARTICALS; j++)
 		{
 			fireworks[i].explotion[j].life = LIFE_LENGHT;
@@ -78,7 +77,7 @@ void DrawTail(FIREWORK s)
 {
 	int j;
 	double intensity = 1;
-		glPointSize(2);
+	glPointSize(2);
 	glBegin(GL_POINTS);
 	for (j = 0;j < TAIL_LENGTH;j++)
 	{
@@ -128,6 +127,135 @@ void DrawSky()
 	DrawStars();
 }
 
+void drawBuilding(double x, double y, double width, double step, int numStores, int numWindowsPerStore)
+{
+	int store, col, style;
+	double dh, dv; // horizontal end vertical spacings
+	
+	double wr = 0.75, wg = 0.75, wb = 0.10; // warm, less bright
+
+	//rectengle
+	glBegin(GL_POLYGON);
+	glColor3d(0.2, 0.2, 0.2);
+	glVertex2d(x + width, y);
+	glVertex2d(x, y);
+	glColor3d(	0.3 + 0.3 * fabs(sin(x * 5)),
+				0.3 + 0.3 * fabs(sin(x * 3)),
+				0.3 + 0.4 * fabs(cos(x * 4)));
+	glVertex2d(x, -0.2);
+	glVertex2d(x + width, -0.2);
+	glEnd();
+
+	//windows
+	double bottom = -0.2;
+	double top = y;
+	double h = top - bottom;          
+	double pad = 0.02 * h;
+	int rows = numStores;
+
+	dh = width / numWindowsPerStore;
+	dv = (h - 2 * pad) / (rows - 1);
+	style = ((int)floor((x + 1.0) / (width + step))) % 5;
+	
+	switch (style) {
+	case 0: 
+		glPointSize(2);
+		glColor3d(wr, wg, wb);
+		for (store = 0; store < numStores; store++) {
+			for (col = 0; col < numWindowsPerStore; col++) {
+				glBegin(GL_POINTS);
+				glVertex2d(x + col * dh, -0.2 + store * dv);
+				glEnd();
+			}
+		}
+		break;
+
+	case 1: 
+		glPointSize(2);
+		glColor3d(wr, wg, wb);
+		for (store = 0; store < numStores; store++) {
+			double offset = (store % 2) ? 0.5 * dh : 0.0;
+			for (col = 0; col < numWindowsPerStore; col++) {
+				glBegin(GL_POINTS);
+				glVertex2d(x + offset + col * dh, -0.2 + store * dv);
+				glEnd();
+			}
+		}
+		break;
+
+	case 2:
+		glPointSize(2);
+		glColor3d(wr, wg, wb);
+		for (store = 0; store < numStores; store++) {
+			for (col = 0; col < numWindowsPerStore; col++) {
+				if ((store + col) % 2) continue;
+				glBegin(GL_POINTS);
+				glVertex2d(x + col * dh, -0.2 + store * dv);
+				glEnd();
+			}
+		}
+		break;
+
+	case 3:
+	{
+		double hseg = dv * 0.45; // window height
+		glColor3d(wr * 0.95, wg * 0.95, wb); // slightly darker
+		for (store = 0; store < numStores; store++) {
+			for (col = 0; col < numWindowsPerStore; col++) {
+				double cx = x + col * dh;
+				double cy = -0.2 + store * dv;
+				glBegin(GL_LINES);
+				glVertex2d(cx, cy - hseg * 0.5);
+				glVertex2d(cx, cy + hseg * 0.5);
+				glEnd();
+			}
+		}
+	}
+	break;
+
+	case 4:
+	{
+		int r2 = numStores / 2;
+		int c2 = numWindowsPerStore / 2;
+		double dh2 = width / c2;
+		double dv2 = (y + width + step) / r2;
+
+		glPointSize(3); // bigger dots
+		glColor3d(wr * 0.9, wg * 0.9, wb);
+		for (store = 0; store < r2; store++) {
+			for (col = 0; col < c2; col++) {
+				glBegin(GL_POINTS);
+				glVertex2d(x + col * dh2, -0.2 + store * dv2);
+				glEnd();
+			}
+		}
+	}
+	break;
+	}
+	/*glPointSize(2);
+	glColor3d(1, 1, 0);
+	for (store = 0; store < numStores; store++)
+	{
+		for (col = 0; col < numWindowsPerStore; col++)
+		{
+			glBegin(GL_POINTS);
+			glVertex2d(x + col * dh, -0.2 + store * dv);
+			glEnd();
+		}
+	}*/
+
+	//Bulding Outline
+	glBegin(GL_LINE_LOOP);
+	glColor3d(0, 0, 0);
+	glVertex2d(x, y);
+	glVertex2d(x, -0.2);
+	glVertex2d(x + width, -0.2);
+	glColor3d(1, 1, 1);
+	glVertex2d(x + width, y);
+	glEnd();
+
+}
+
 void DrawBackBuildings()
 {
 	double x, y;
@@ -136,7 +264,7 @@ void DrawBackBuildings()
 
 	// for windows
 	int store, col;
-	int numStores = 25;
+	int numStores = 30;
 	int numWindowsPerStore = 10;
 	double dh, dv; // horizontal end vertical spacings
 
@@ -144,105 +272,26 @@ void DrawBackBuildings()
 	for (x = -1; x <= 1; x += step)
 	{
 		y = 0.3 * cos(0.3 * PI * x) + 0.15 * sin(x * 17 * PI) + 0.2 * sin(x * 5 * PI);
-		//Building Rectengle
-		glBegin(GL_POLYGON);
-		glColor3d(0.2, 0.2 * x, 0.2);
-		glVertex2d(x + width, y);
-		glVertex2d(x, y);
-		glColor3d(0.3,  fabs(y), 0.3 * y);
-		glVertex2d(x, -0.2);
-		glVertex2d(x + width, -0.2);
-		glEnd();
-
-		//Windows
-		dh = width / numWindowsPerStore;
-		dv = (y + width + step * 4) / numStores;
-		glPointSize(2);
-		glColor3d(1, 1, 0);
-		for (store = 0; store < numStores; store++)
-		{
-			for (col = 0; col < numWindowsPerStore; col++)
-			{
-				glBegin(GL_POINTS);
-				glVertex2d(x + col * dh, -0.2 + store * dv);
-				glEnd();
-			}
-		}
-
-		//Bulding Outline
-		glBegin(GL_LINE_LOOP);
-		glColor3d(0, 0, 0);
-		glVertex2d(x, y);
-		glVertex2d(x, -0.2);
-		glVertex2d(x + width, -0.2);
-		glColor3d(1, 1, 1);
-		glVertex2d(x + width, y);
-		glEnd();
-
-		//Reflections
-
-		
-
+		drawBuilding(x, y, width, step, numStores, numWindowsPerStore);
 		x += width;
 	}
 
 }
 
-
 void DrawFrontBuildings()
 {
 	double x, y;
+	int store, col;
 	double width = 0.15;
 	double step = 0.05;
-	
-	// for windows
-	int store, col;
 	int numStores = 20;
 	int numWindowsPerStore = 10;
-	double dh, dv; // horizontal end vertical spacings
-	
-	// near buildings 
+
 	for (x = -1; x <= 1; x += step)
 	{
-
 		y = 0.3 * cos(0.3 * PI * x) + 0.1 * sin(x * 50 * PI) + 0.1 * sin(x * 12 * PI);
-		//Building Rectengle
-		glBegin(GL_POLYGON);
-		glColor3d(0.2, 0.2, 0.2);
-		glVertex2d(x + width, y);
-		glVertex2d(x, y);
-		glColor3d(0.8, 0.8 , 0.7);
-		glVertex2d(x, -0.2);
-		glVertex2d(x + width, -0.2);
-		glEnd();
-
-		//Windows
-		dh = width / numWindowsPerStore;
-		dv = (y + width + step) / numStores;
-		glPointSize(2);
-		glColor3d(1, 1, 0);
-		for (store = 0; store < numStores; store++)
-		{
-			for (col= 0; col < numWindowsPerStore; col++)
-			{
-				glBegin(GL_POINTS);
-					glVertex2d(x + col * dh, -0.2 + store * dv);
-				glEnd();
-			}
-		}
-		
-		//Bulding Outline
-		glBegin(GL_LINE_LOOP);
-		glColor3d(0, 0, 0);
-		glVertex2d(x, y);
-		glVertex2d(x, -0.2);
-		glVertex2d(x + width, -0.2);
-		glColor3d(1, 1, 1);
-		glVertex2d(x + width, y);
-		glEnd();
-
+		drawBuilding(x, y, width, step, numStores, numWindowsPerStore);
 		x += width;
-
 	}
 }
 
@@ -250,10 +299,10 @@ void DrawAxis()
 {
 	glColor3d(1, 1, 1);
 	glBegin(GL_LINES);
-		glVertex2d(-1, 0);
-		glVertex2d(1, 0);
-		glVertex2d(0, 1);
-		glVertex2d(0, -1);
+	glVertex2d(-1, 0);
+	glVertex2d(1, 0);
+	glVertex2d(0, 1);
+	glVertex2d(0, -1);
 	glEnd();
 }
 
@@ -261,7 +310,7 @@ void DrawFireworks()
 {
 	int i, j;
 
-	glColor3d(1, 1, 1); 
+	glColor3d(1, 1, 1);
 
 	for (i = 0;i < NUM_FIREWORKS;i++)
 	{
@@ -300,11 +349,11 @@ void display()
 	DrawBackBuildings();
 	DrawFrontBuildings();
 
-	DrawAxis();
+	//DrawAxis();
 	glutSwapBuffers(); // show all
 }
 
-void idle() 
+void idle()
 {
 	int index;
 	int i, j, allDead;
@@ -315,7 +364,7 @@ void idle()
 		index = rand() % NUM_STARS;
 		stars[index].size = 1 + rand() % 3;
 	}
-	
+
 	//firework going up
 	for (i = 0; i < NUM_FIREWORKS; i++)
 	{
@@ -332,7 +381,7 @@ void idle()
 		{
 			// setting the ground for the explotion
 			fireworks[i].expload = 1;
-			
+
 			for (j = 0; j < NUM_PARTICALS; j++)
 			{
 				fireworks[i].explotion[j].x = fireworks[i].position.x; //settings the location of the explotion
@@ -340,7 +389,7 @@ void idle()
 				fireworks[i].explotion[j].life = LIFE_LENGHT;
 			}
 		}
-		
+
 		//the explotion
 		if (fireworks[i].expload)
 		{
@@ -379,13 +428,13 @@ void idle()
 	glutPostRedisplay(); // indirect call to display
 }
 
-void main(int argc, char* argv[]) 
+void main(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE); // defines visual memory
 	glutInitWindowSize(600, 600);
 	glutInitWindowPosition(400, 100);
-	glutCreateWindow("City skyline"); 
+	glutCreateWindow("City skyline");
 	glutDisplayFunc(display); // this is a pointer to refresh window function
 	glutIdleFunc(idle); // this is a pointer to idle function that runs in background all the time
 
